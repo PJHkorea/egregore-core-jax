27498bb38c30fad45d40bfa55bbee8cf3f677ae6
PJHkorea <zenoforever@naver.com>
2026-07-04 17:25:02 +0900

    Update math_guardrails.py

--- egregore_core_jax/math_guardrails.py
+++ egregore_core_jax/math_guardrails.py
@@ -1,3 +1,7 @@
+# ====================================================================
+# Copyright (c) 2026 PJHkorea | Licensed under the Apache License, Version 2.0
+# ====================================================================
+
 import jax
 import jax.numpy as jnp
 
