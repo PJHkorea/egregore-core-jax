10faa63750675413f000a097928159ab1f54e5b5
PJHkorea <zenoforever@naver.com>
2026-07-04 17:25:11 +0900

    Update optimizers.py

--- egregore_core_jax/optimizers.py
+++ egregore_core_jax/optimizers.py
@@ -1,3 +1,7 @@
+# ====================================================================
+# Copyright (c) 2026 PJHkorea | Licensed under the Apache License, Version 2.0
+# ====================================================================
+
 import jax
 import optax
 from typing import Optional, Callable, Union
